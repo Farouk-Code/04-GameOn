afda4e3dfad644b4b706931508fa2011adb7f767
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-03-22 18:18:47 +0100

    Form validation function

--- modal.js
+++ modal.js
@@ -8,16 +8,48 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+const closeModal = document.querySelectorAll(".close");
+const content = document.querySelector(".content");
+const testModal = document.querySelector(".postRegisterModal");
+const testClose = document.querySelector(".postRegisterClose");
+const btnClose = document.querySelector(".btn-close");
+const testContent = document.querySelector(".postRegisterContent");
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+const launchModal = () => {
+  modalBg.style.display = "block";
+};
+
+// Launch modal event
+for (let btn of modalBtn) {
+  btn.addEventListener("click", launchModal);
 }
 
+// Close modal Form
+const handleCloseModal = () => {
+  content.classList.add("hide-modal");
+  modalBg.style.display = "none";
+  content.classList.remove("hide-modal");
+};
 
+for (let element of closeModal) {
+  element.addEventListener("click", handleCloseModal);
+}
+
+// Form Validation
+
+const validateName = (input, errorElement) => {
+  const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/;
+  if (regex.test(input.value)) {
+    errorElement.style.display = "none";
+    input.classList.remove("field-error");
+    return true;
+  } else {
+    errorElement.style.display = "block";
+    input.classList.add("field-error");
+    return false;
+  }
+};
